10aa4a3153244b33d8894391f96fe2c76748beb9
Laynerker <laynerker.gdl@gmail.com>
2026-02-01 23:12:51 -0300

    Revert "update for deploy"
    This reverts commit 0eed7ff4ddb4344389bd6ba28f6627499a989675.

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    images: { unoptimized: true },
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
